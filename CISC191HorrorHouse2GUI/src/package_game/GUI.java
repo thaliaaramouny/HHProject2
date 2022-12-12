d9afa338838562c9b6c424bec43aab483178ebcb
annamairaj <108256332+annamairaj@users.noreply.github.com>
2022-12-12 10:16:07 -0800

    Update GUI.java
    lil changes w/ images

--- CISC191HorrorHouse2GUI/src/package_game/GUI.java
+++ CISC191HorrorHouse2GUI/src/package_game/GUI.java
@@ -28,7 +28,7 @@ public class GUI
 	JPanel imagePanel, imagePanel2, imagePanel3, imagePanel4, imagePanel5, imagePanel6, imagePanel7, imagePanel8, imagePanel9, imagePanel10, imagePanel11;
 	JLabel title, timerLabel, timerCount, healthLabel, healthCount, img1, img2, img3, img4, img5, img6, img7, img8, img9, img10, img11, img12, pinLabel;
 	JTextField txt1;
-	JButton startButton, move1, move2, viewCouch, skipCouch, enterKitchen, viewDrawer, readSheet, pickUpBook, enterPin;
+	JButton startButton, move1, move2, viewCouch, skipCouch, enterKitchen, viewDrawer, readSheet, pickUpBook, enterPin, viewPaper;
 	ButtonGroup bg1;
 	JTextArea textArea;
 	JTextField pin; //for pin
@@ -107,14 +107,14 @@ public class GUI
 		img3 = new JLabel();
 		img3.setIcon(new ImageIcon("Images/LivingRoom.png"));
 		//Close up couch 
-	//	img4 = new JLabel();
-	//	img4.setIcon(new ImageIcon("Images/Couch.png"));
+		img4 = new JLabel();
+		img4.setIcon(new ImageIcon("Images/Couch.png"));
 		//Newspaper
 		img5 = new JLabel();
 		img5.setIcon(new ImageIcon("Images/note.png"));
 		//Kitchen
-	//	img6 = new JLabel();
-	//	img6.setIcon(new ImageIcon("Images/KitchenDoor.png"));
+		img6 = new JLabel();
+		img6.setIcon(new ImageIcon("Images/KitchenDoor.png"));
 		//Zoom in drawer 
 		img7 = new JLabel();
 		img7.setIcon(new ImageIcon("Images/Cabinate.png"));
@@ -128,63 +128,54 @@ public class GUI
 		img10 = new JLabel();
 		img10.setIcon(new ImageIcon("Images/ExitDoor.png"));
 		//Win Screen 
-	//	img11 = new JLabel();
-	//	img11.setIcon(new ImageIcon("Images/WinScreen.png"));
+		img11 = new JLabel();
+		img11.setIcon(new ImageIcon("Images/WinScreen.png"));
 		
 		
 		
 		
-		//image panel1
+		//image panel1 (FD)
 		imagePanel= new JPanel();
 		imagePanel.add(img2);
 		imagePanel.setBackground(Color.black);
-		//image panel 2
+		//image panel 2 (LR)
 		imagePanel2 = new JPanel();
 		imagePanel2.add(img3);
 		imagePanel2.setBackground(Color.black);
-		//image panel 3
+		//image panel 3 (COUCH)
 		imagePanel3 = new JPanel();
-		imagePanel3.add(img5);
+		imagePanel3.add(img4);
 		imagePanel3.setBackground(Color.black);
-		//image panel 4
+		//image panel 4 (NOTE)
 		imagePanel4 = new JPanel();
-		imagePanel4.add(img7);
+		imagePanel4.add(img5);
 		imagePanel4.setBackground(Color.black);
-		//image panel 5
-//		imagePanel5 = new JPanel();
-//		imagePanel5.add(img6);
-//		imagePanel5.setBackground(Color.black);
-//		//image panel 6
-//		imagePanel6 = new JPanel();
-//		imagePanel6.add(img7);
-//		imagePanel6.setBackground(Color.black);
-//		//image panel 7
-//		imagePanel7 = new JPanel();
-//		imagePanel7.add(img8);
-//		imagePanel7.setBackground(Color.black);
-//		//image panel 8
-//		imagePanel8 = new JPanel();
-//		imagePanel8.add(img9);
-//		imagePanel8.setBackground(Color.black);
-//		//image panel 9
-//		imagePanel9 = new JPanel();
-//		imagePanel9.add(img10);
-//		imagePanel9.setBackground(Color.black);
-//		//image panel 10
-//		imagePanel10 = new JPanel();
-//		imagePanel10.add(img11);
-//		imagePanel10.setBackground(Color.black);
-//		//image panel 11
-//		imagePanel11 = new JPanel();
-//		imagePanel11.add(img12);
-//		imagePanel11.setBackground(Color.black);
-//		//image panel 12
-//		imagePanel12 = new JPanel();
-//		imagePanel12.add(img13);
-//		imagePanel12.setBackground(Color.black);
-		
-		
-		
+		//image panel 5 (KD)
+		imagePanel5 = new JPanel();
+		imagePanel5.add(img6);
+		imagePanel5.setBackground(Color.black);
+		//image panel 6 (CABINET)
+		imagePanel6 = new JPanel();
+		imagePanel6.add(img7);
+		imagePanel6.setBackground(Color.black);
+		//image panel 7 (DRAWER)
+		imagePanel7 = new JPanel();
+		imagePanel7.add(img8);
+		imagePanel7.setBackground(Color.black);
+		//image panel 8 (FORMULA)
+		imagePanel8 = new JPanel();
+		imagePanel8.add(img9);
+		imagePanel8.setBackground(Color.black);
+		//image panel 9 (EXIT)
+		imagePanel9 = new JPanel();
+		imagePanel9.add(img10);
+		imagePanel9.setBackground(Color.black);
+		//image panel 10 (WIN)
+		imagePanel10 = new JPanel();
+		imagePanel10.add(img11);
+		imagePanel10.setBackground(Color.black);
+
+
 		///// Title Screen /////
 		//creating custom fonts to use
 		titleFont = new Font("Times New Roman", Font.BOLD, 90);
@@ -251,7 +242,15 @@ public class GUI
 		viewCouch.setVisible(false);
 		bottomPanel.add(viewCouch);
 		
-		//From couch to kitchen 
+		//BUTTON 4
+		viewPaper = new JButton("View Paper");
+		viewPaper.setBackground(Color.black);
+		viewPaper.setFont(buttonFont);
+		viewPaper.setForeground(Color.red);
+		viewPaper.setVisible(false);
+		bottomPanel.add(viewPaper);
+		
+		//BUTTON 5
 		enterKitchen = new JButton("Enter Kitchen");
 		enterKitchen.setBackground(Color.black);
 		enterKitchen.setFont(buttonFont);
@@ -259,7 +258,7 @@ public class GUI
 		enterKitchen.setVisible(false);
 		bottomPanel.add(enterKitchen);
 		
-		//button 5
+		//BUTTON 6
 		viewDrawer = new JButton("View Drawer");
 		viewDrawer.setBackground(Color.black);
 		viewDrawer.setFont(buttonFont);
@@ -267,7 +266,7 @@ public class GUI
 		viewDrawer.setVisible(false);
 		bottomPanel.add(viewDrawer);
 		
-		//button6
+		//BUTTON 7
 		readSheet = new JButton("Read formula sheet");
 		readSheet.setBackground(Color.black);
 		readSheet.setFont(buttonFont);
@@ -275,7 +274,7 @@ public class GUI
 		readSheet.setVisible(false);
 		bottomPanel.add(readSheet);
 		
-		//button 7
+		//BUTTON 8
 		pickUpBook = new JButton("Pick up book");
 		pickUpBook.setBackground(Color.black);
 		pickUpBook.setFont(buttonFont);
@@ -283,7 +282,7 @@ public class GUI
 		pickUpBook.setVisible(false);
 		bottomPanel.add(pickUpBook);
 		
-		//final button
+		//FINAL BUTTON
 		enterPin = new JButton("ENTER PIN");
 		enterPin.setBackground(Color.black);
 		enterPin.setFont(buttonFont);
@@ -313,6 +312,7 @@ public class GUI
 		readSheet.addActionListener(BL);
 		pickUpBook.addActionListener(BL);
 		enterPin.addActionListener(BL);
+		viewPaper.addActionListener(BL);
 		
 		//
 		
@@ -321,10 +321,7 @@ public class GUI
 		window.setVisible(true);
 		
 	}
-	
-	
 
-	
 	//button listener for all buttons
 	private class ButtonListener implements ActionListener
 	{
@@ -412,12 +409,11 @@ public class GUI
 					+"An Over-head speaker starts to speak, and he hears.\n"
 					+"WELCOME YOUNG ONE. YOU HAVE ENTERED MY HORROR HOUSE,\n" 
 					+"AS I SPEAK THE HOUSE IS BEING PUMPED WITH NITROUS OXIDE.\n"
-					+"YOU WILL DIE IN EXACTLY 15 MINUTES. HAHAHA\n"
+					+"YOU WILL DIE IN EXACTLY 3 MINUTES. HAHAHA\n"
 					+"'FIND THEM FAST AND INPUT YOUR CODE INTO THE KEYPAD'\n"
 					+"The child looks around and realizes he walked straight\n"
 					+"into an old dark living room.\n"
 					+"He decides to look around for any clues to help him escape.");
-				
 				
 			}
 			
@@ -430,34 +426,58 @@ public class GUI
 			}
 			
 			if (e.getSource() == viewCouch)
-			{
+			{	
 				//image
 				window.remove(imagePanel2);
 				window.add(imagePanel3);
 				imagePanel3.add(textArea);
 				
+				//buttons
+				enterKitchen.setVisible(false);
 				viewCouch.setVisible(false);
-				viewCouch.setVisible(false);
+				
+				//text
 				textArea.setFont(textFont1);
 				textArea.setText("Hmm, what is that in the corner of the couch?\n"
-				+"The child takes a closer look to see an old crumpled newspaper \n"
-				+ "wedged in between the dirty cushions.It might have something helpful!\n"
-				+ "Its a newspaper stained with bloody fingerprints\n"
-				+ "... \n"
-				+"Wow, well 31 seems to be an important number here! \n"
-				+ "You might want to hold on to it.");
-				enterKitchen.setVisible(true);
+				+"The child takes a closer look to see an old crumpled paper \n"
+				+ "wedged in between the dirty cushions.It might have something helpful!");
+				viewPaper.setVisible(true);
 			}
 			
-			if (e.getSource() == enterKitchen)
+			if (e.getSource() == viewPaper)
 			{
 				//image
 				window.remove(imagePanel3);
 				window.add(imagePanel4);
 				imagePanel4.add(textArea);
 				
+				//buttons
+				viewPaper.setVisible(false);
 				
+				//text
+				textArea.setFont(textFont1);
+				textArea.setText("Dates and times are important, think twice about\n" 
+				+"your findings and you may be surprised\n"
+				+"Hmm what can be so important about this date?\n"
+				+"March 14th, 3/14, a 3 digit number….\n"
+				+"Time is running out, beat it or prepare for your demise");
+				enterKitchen.setVisible(true);
+	
+			}
+				
+			if (e.getSource() == enterKitchen)
+			{
+				//image
+				window.remove(imagePanel3);
+				window.remove(imagePanel2);
+				window.add(imagePanel6);
+				imagePanel6.add(textArea);
+				
+				//buttons
 				viewCouch.setVisible(false);
+				enterKitchen.setVisible(false);
+				
+				//text
 				textArea.setFont(textFont1);
 				textArea.setText("The child walks around in the kitchen and sees \n"
 						+ "an open junk drawer \n");
@@ -467,17 +487,36 @@ public class GUI
 			
 			}
 			
-			if (e.getSource()== viewDrawer) {
+			if (e.getSource()== viewDrawer) 
+			{
+				//image
+				window.remove(imagePanel6);
+				window.add(imagePanel7);
+				imagePanel7.add(textArea);
+				
+				//buttons
 				viewDrawer.setVisible(false);
 				readSheet.setVisible(true);
 				pickUpBook.setVisible(true);
+				
+				//text
 				textArea.setText("The junk drawer is filled with various books,\n"
 						+"and sheets and sheets of formulas for\n"
 						+"different chemical compounds.");
 			}
 			
-			if (e.getSource()== readSheet) {
+			if (e.getSource()== readSheet) 
+			{
+				//image
+				window.remove(imagePanel7);
+				window.add(imagePanel8);
+				imagePanel8.add(textArea);
+				
+				//buttons
 				readSheet.setVisible(false);
+				pickUpBook.setVisible(false);
+				
+				//tex
 				textArea.setText("Ahh, chemical compounds and using the scientific method,\n"
 						+ " nothing here for you, but you better find a digit\n"
 						+ " before you get lightheaded!\n");
@@ -485,32 +524,44 @@ public class GUI
 			}
 			
 			if(e.getSource() == pickUpBook) {
-				pickUpBook.setVisible(false);
-				readSheet.setVisible(false);
-				textArea.setText("It reads'Evil Science: 1st Edition, \n"
-						+ "Publishing date: March 14th, 1880'	\n"
-						+ "Dates and times are important, think twice \n"
-						+ "about your findings and you may be surprised. \n"
-						+ "Hmm what can be so important about this date? \n"
-						+ "March 14th, 3/14, a 3 digit number�\n"
-						+ "Time is running out, beat it or prepare for your demise\n");
-				enterPin.setVisible(true);
+//				//image
+//				window.remove(imagePanel7);
+//				window.add(imagePanel7);
+//				
+//				
+//				//buttons
+//				imagePanel7.add(textArea);
+//				pickUpBook.setVisible(false);
+//				readSheet.setVisible(false);
+//				
+//				//text
+//				textArea.setText("It reads'Evil Science: 1st Edition, \n"
+//						+ "Publishing date: March 14th, 1880'	\n"
+//						+ "Dates and times are important, think twice \n"
+//						+ "about your findings and you may be surprised. \n"
+//						+ "Hmm what can be so important about this date? \n"
+//						+ "March 14th, 3/14, a 3 digit number�\n"
+//						+ "Time is running out, beat it or prepare for your demise\n");
+//				enterPin.setVisible(true);
 			}
 			
 			if(e.getSource() == enterPin) 
 			{
-				textArea.setVisible(false);
-				pin.setEditable(true);
-				pinLabel.setVisible(true);
-				pin.setVisible(true);
-				
-				enterPin.setVisible(false);
-				
-				
+//				window.remove(imagePanel7);
+//				window.add(imagePanel9);
+//				
+//				imagePanel.add(textArea);
+//				textArea.setVisible(false);
+//				pin.setEditable(true);
+//				pinLabel.setVisible(true);
+//				pin.setVisible(true);
+//				enterPin.setVisible(false);
+//				
 			}
 		}
 	
 	}
+
 	
 	private class textFieldListener implements ActionListener
 	{
@@ -520,7 +571,8 @@ public class GUI
 		{
 			
 			userPin = Integer.parseInt(pin.getText());
-			if(userPin == correctPin) {
+			if(userPin == correctPin) 
+			{
 
 				textArea.setText(" A GHOSTLY VOICE FILLS THE ROOM: �NOOOO, COME BACK, COME BACKKKKK�\n"
 						+ "As the door opens the child runs, and runs, runs. As the house shrinks in \n"
@@ -542,7 +594,8 @@ public class GUI
 				textArea.setVisible(true);	
 			}
 			
-			if(userPin != correctPin) {
+			if(userPin != correctPin) 
+			{
 				textArea.setText("A GHOSTLY VOICE FILLS THE ROOM: \n"
 						+ "�HAHAHA, YOU COULD NEVER ESCAPE ME! \n"
 						+ "MY CHILD, I�M SO SORRY FOR WHAT I HAVE TURNED YOU INTO, \n"
@@ -555,5 +608,7 @@ public class GUI
 			System.out.println("check");
 			
 		}
-	}		
+		{		
+		}
+	}
 }
